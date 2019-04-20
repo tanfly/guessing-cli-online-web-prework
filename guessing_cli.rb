f3b6c577db03c05f2c366eee2a54964efe213d69
Stella McEwen <mcewen.stella@gmail.com>
2019-04-20 22:02:33 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,7 @@ def run_guessing_game
   number = rand
   puts "Pick a number"
   user_input = gets.chomp 
+  while user_input != "exit"
   if user_input == number
     puts "You guessed the correct number!"
   else
