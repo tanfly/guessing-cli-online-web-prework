fe391da4975767f37810e60513de4c1331e6e7e0
Stella McEwen <mcewen.stella@gmail.com>
2019-04-20 22:27:50 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,7 +11,8 @@ def run_guessing_game
   number = rand(1..6)
   if user_input == number
     puts "You guessed the correct number!"
-    puts 
+    puts "Guess a number between 1 and 6"
+    user_input = 
   elsif user_input != number
     puts "The computer guessed #{number}."
     elsif user_input == "exit"
