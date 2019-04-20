bc3746e3ad8d2ab7922ad094ed87ee4ae610e75c
Stella McEwen <mcewen.stella@gmail.com>
2019-04-20 22:00:11 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,10 +4,9 @@ def run_guessing_game
   user_input = gets.chomp 
   if user_input == number
     puts "You guessed the correct number!"
-  else
+  elsif
     puts "The computer guessed #{number}."
-  break
-    if user_input == "exit"
+    elsif user_input == "exit"
       break
     end
   end
